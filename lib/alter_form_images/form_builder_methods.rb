80ac107f722e8d165d50d688ca6fe0fd4b37106c
sergio1990 <sergeg1990@gmail.com>
2013-12-02 17:27:25 +0200

    --

--- lib/alter_form_images/form_builder_methods.rb
+++ lib/alter_form_images/form_builder_methods.rb
@@ -3,12 +3,32 @@ module AlterFormImages
 
     def image_fields_for(method, options={})
       options = default_opts_image_fields options
-      @template.content_tag :div, class: 'alter-image-block' do
-        img_image_fields(options) + input_image_fields(options, method)
-      end
+      #@template.content_tag :div, class: 'alter-image-block' do
+      #  img_image_fields(options) + input_image_fields(options, method)
+      #end
+      render_image_fields method, options
     end
 
     private
+
+    def is_new_record_object?(method)
+      @object.send(method) && !@object.new_record?
+    end
+
+    def render_image_fields(method, options)
+      @template.content_tag :div, class: 'alter-image-block' do
+        is_new_record_object? ? render_new_img_fields(method, options) : render_edit_img_fields(method, options)
+        input_image_fields(options, method)
+      end
+    end
+
+    def render_new_img_fields(method, options)
+
+    end
+
+    def render_edit_img_fields(method, options)
+
+    end
 
     def img_image_fields(options)
       @template.image_tag(options[:default_image], class: 'alter-image-preview', style: "width: #{options[:width]}px; height: #{options[:height]}px;")
