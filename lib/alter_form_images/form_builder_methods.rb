27c83c05773e18d88e6931ae5c7b592fda0e7160
sergio1990 <sergeg1990@gmail.com>
2013-12-03 09:59:13 +0200

    -Added editing image

--- lib/alter_form_images/form_builder_methods.rb
+++ lib/alter_form_images/form_builder_methods.rb
@@ -3,46 +3,43 @@ module AlterFormImages
 
     def image_fields_for(method, options={})
       options = default_opts_image_fields options
-      #@template.content_tag :div, class: 'alter-image-block' do
-      #  img_image_fields(options) + input_image_fields(options, method)
-      #end
       render_image_fields method, options
     end
 
     private
 
     def is_new_record_object?(method)
-      @object.send(method) && !@object.new_record?
+      @object.new_record? || @object.send(method).nil?
     end
 
     def render_image_fields(method, options)
       @template.content_tag :div, class: 'alter-image-block' do
-        is_new_record_object? ? render_new_img_fields(method, options) : render_edit_img_fields(method, options)
-        input_image_fields(options, method)
+        img_image_fields(method, options) + input_image_fields(method, options)
       end
     end
 
-    def render_new_img_fields(method, options)
-
+    def img_image_fields(method, options)
+      @template.image_tag(preview_img_url(method, options, is_new_record_object?(method)), class: 'alter-image-preview', style: create_img_styles(options))
     end
 
-    def render_edit_img_fields(method, options)
-
+    def preview_img_url(method, options, is_new_record)
+      is_new_record ? options[:default_image] : @object.send(method).url(options[:preview_style])
     end
 
-    def img_image_fields(options)
-      @template.image_tag(options[:default_image], class: 'alter-image-preview', style: "width: #{options[:width]}px; height: #{options[:height]}px;")
-    end
-
-    def input_image_fields(options, method)
+    def input_image_fields(method, options)
       @template.file_field_tag("#{@object_name}[#{method}]", class: 'alter-image-input')
+    end
+
+    def create_img_styles(options)
+      "width: #{options[:width]}px; height: #{options[:height]}px;"
     end
 
     def default_opts_image_fields(options)
       opts = {
           width: 100,
           height: 100,
-          default_image: 'alter_default_image.png'
+          default_image: 'alter_default_image.png',
+          preview_style: :original
       }
       options.merge(opts)
     end
